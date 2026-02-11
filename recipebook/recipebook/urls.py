3433acd78a75fc8e8356648fbc5488d5d08172a9
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-02-11 13:04:31 +0800

    updated urls to remove ledger/ in path

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ledger/', include('ledger.urls')),
+    path('', include('ledger.urls')),
 ]

989a13ed5c667a7d2595538a02d17e1b9e977866
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-02-19 09:10:31 +0800

    created Ingredient, Recipe, and RecipeIngredient models in models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=5)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+        )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+        )

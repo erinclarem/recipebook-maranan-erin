192ab577c63c6ff779d6798866022404021ca866
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-02-19 17:59:36 +0800

    Removed unnecessary code in urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    # path('recipes/list', recipes_list, name='recipes_list'),
-    # path('recipe/1', recipe1, name='recipe1'),
-    # path('recipe/2', recipe2, name='recipe2'),
     path('recipes/list', RecipeListView.as_view(), name='recipes_list'),
     path('<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
 ]

6e05480b3456a2f92a7b035892c560ac1e8a3ad7
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-03-02 17:13:56 +0800

    Updated Profile bio
    made bio require more than 255 characters

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(
+        validators=[MinLengthValidator(256)],
+        blank=True
+        )
 
     def __str__(self):
         return f"{self.name}"
